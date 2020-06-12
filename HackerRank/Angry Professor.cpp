bcb1ae1e5df48ee5016ab736b6aa724b76b76b8b
Shardul Singh <41698288+shardul1999@users.noreply.github.com>
2020-06-12 10:40:36 +0530

    Create Angry Professor.cpp

---
+++ HackerRank/Angry Professor.cpp
@@ -0,0 +1,59 @@
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define FIO             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+//mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define ll long long
+
+#define MIN(a, b) a < b ? a : b
+#define MAX(a, b) a > b ? a : b
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+string solve(int a[],int n,int k)
+{
+    int count1=0;
+    for(int i=0;i<n;i++)
+        if(a[i]<=0)
+        count1++;
+    if(count1>=k)
+        return "NO";
+    else
+        return "YES";
+
+
+}
+int32_t main()
+{
+    FIO;
+    w(t)
+    {
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        cout<<solve(a,n,k)<<"\n";
+    }
+
+    return 0;
+}
