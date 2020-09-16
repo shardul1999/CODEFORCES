f884b0bc46c3717aee771de874b79bcdf4dcad1e
Shardul Singh <41698288+shardul1999@users.noreply.github.com>
2020-09-16 11:43:55 +0530

    Create Overlapping Rectangles.cpp

---
+++ Coding Blocks/FAANG/Overlapping Rectangles.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define FIO             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+//mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define ll long long
+ 
+#define MIN(a, b) a < b ? a : b
+#define MAX(a, b) a > b ? a : b
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+int32_t main()
+{
+    int x1,y1,x2,y2,x3,y3,x4,y4;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
+    int y=1;
+    if(x2<=x3 || x4<=x1)
+        y=0;
+    if(y2<=y3 || y4<=y1)
+        y=0;
+    if(y)
+        cout<<1;
+    else
+        cout<<0;
+    
+    return 0;
+}
